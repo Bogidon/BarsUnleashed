6139c3b6aabd8baf8f847b0a2abdcfc39d8b63db
Bogdan Vitoc <bogdanvitoc@gmail.com>
2016-04-16 21:09:17 -0400

    Initial Commit

---
+++ BarsUnleashed/BarsUnleashed.h
@@ -0,0 +1,19 @@
+//
+//  BarsUnleashed.h
+//  BarsUnleashed
+//
+//  Created by Bogdan Vitoc on 4/16/16.
+//  Copyright © 2016 Unleash.me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BarsUnleashed.
+FOUNDATION_EXPORT double BarsUnleashedVersionNumber;
+
+//! Project version string for BarsUnleashed.
+FOUNDATION_EXPORT const unsigned char BarsUnleashedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BarsUnleashed/PublicHeader.h>
+
+
